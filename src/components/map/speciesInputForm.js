682194ad2a2715fee179ffbc78585a53d780cae1
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2019-09-24 20:18:21 -0700

    added half the form fields to the species component

--- src/components/map/speciesInputForm.js
+++ src/components/map/speciesInputForm.js
@@ -7,16 +7,61 @@ export default class SpeciesInputForm extends React.Component {
   }
 
   render() {
+
+    // break into different forms - modular 
+    // finish adding the rest of the info 
     return (
       <div id='species-input-container'>
-        <form id='species-input-form'>
+        <form id='species-name-form'>
           <label>Species Name</label>
           <input type='text' placeholder='Enter Species Name'/>
           <button type='submit'>Search</button>
+        </form>
+        <div>Select Species</div>
+        <ul>
+          <li>category 1</li>  
+          <li>category 2</li>  
+          <li>category 3</li>  
+          <li>category 4</li>  
+        </ul>
+        <form id='species-scientific-name'>
           <label>Species Scientific Name</label>
           <input type='text' placeholder='Enter Scientific Name'/>
           <label>label for all info needed for species information</label>
         </form>
+        <div id='species-category'>Category</div>
+        <ul>
+          <li>category 1</li>  
+          <li>category 2</li>  
+          <li>category 3</li>  
+          <li>category 4</li>  
+        </ul>
+        <div id='species-weight-range'>Weight Range</div>
+        <ul>
+          <li>category 1</li>  
+          <li>category 2</li>  
+          <li>category 3</li>  
+          <li>category 4</li>  
+        </ul>
+        <div id='species-length-range'>Length Range</div>
+        <ul>
+          <li>category 1</li>  
+          <li>category 2</li>  
+          <li>category 3</li>  
+          <li>category 4</li>  
+        </ul>
+        <div id='species-region'>Regions</div>
+        <form>
+          <label>Add Regions</label>
+          <input type='text' placeholder='Add Region'/>
+          <input type='submit'/>
+        </form>
+        <ul>
+          <li>category 1</li>  
+          <li>category 2</li>  
+          <li>category 3</li>  
+          <li>category 4</li>  
+        </ul>
       </div>
     )
   }
