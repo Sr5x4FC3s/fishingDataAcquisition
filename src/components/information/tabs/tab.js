adac6606c7dad14ee889ddd510ba001c6a771710
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2019-09-27 16:45:41 -0700

    added styling changes to tab to center the wording

--- src/components/information/tabs/tab.js
+++ src/components/information/tabs/tab.js
@@ -14,12 +14,26 @@ export default class Tab extends React.Component {
       height: '40px',
       backgroundColor: 'blue',
       opacity: '0.5',
+      position: 'relative',
+    };
+
+    const wordStyle = {
+      float: 'left',
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      marginRight: '-50%',
+      transform: `translate(-50%, -50%)`
+    };
+
+    const closeButtonStyle = {
+      float: 'right',
     };
 
     return (
       <div style={styles}>
-        <div id={`${this.props.value}-tab`}>{this.props.value}</div>
-        <div onClick={this.props.close}>x</div>
+        <div id={`${this.props.value}-tab`} style={wordStyle}>{this.props.value}</div>
+        <div onClick={this.props.close} style={closeButtonStyle}>x</div>
       </div>
     )
   }
