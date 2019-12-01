d655ec07ce25dc43f252e0ddb95c86f1946341ea
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2019-12-01 13:35:53 -0800

    added file dropper and image uploader component

--- src/components/map/menuOptions/addCoordinatesForm.js
+++ src/components/map/menuOptions/addCoordinatesForm.js
@@ -3,8 +3,15 @@ import React, { useState, useEffect } from 'react';
 import FormWithDropDown from '../../forms/formWithDropDown';
 import SaveButton from '../../forms/saveButton';
 import GenericButton from '../../header/button';
+import DisabledButton from '../../header/disabledButton';
 import LocationCard from '../../information/information/cards/locationCard';
-import SpeciesInformtionCard from '../../information/information/cards/speciesInformationCard';
+import SpeciesInformationCard from '../../information/information/cards/speciesInformationCard';
+// import ImageDropZone from '../../photo/dropZone';
+// import PhotoContainer from '../../information/information/container/photoContainer';
+
+//test
+import ImageDropContainer from '../../information/information/container/imageDropContainer';
+
 import { retrieve } from '../../../../utility/apiUtility';
 
 const AddCoordinateForm = (props) => {
@@ -14,6 +21,8 @@ const AddCoordinateForm = (props) => {
   const [locationInformation, setLocationInformation] = useState({});
   const [searchStatus, setStatus] = useState({status: false, failedAttempts: 0,});
   const [catchInformation, changeCatchInformation] = useState(null);
+  const [uploadedImageList, setImageList] = useState([]);
+  const [toggleViewStates, toggleStates] = useState({showImageButton: true, renderFileDrop: false,});
 
   const captureValue = (event, type) => {
     switch(type) {
@@ -67,6 +76,21 @@ const AddCoordinateForm = (props) => {
   const toggle = (information) => {
     changeCatchInformation(information);
     console.log('catch information: ', catchInformation)
+  };
+
+  const toggleViews = (type) => {
+    switch(type) {
+      case('IMAGE_BUTTON'):
+        toggleStates({showImageButton: !toggleViewStates.showImageButton});
+        break;
+      case('FILE_DROP'): 
+        toggleStates({renderFileDrop: !toggleViewStates.renderFileDrop});
+        break;
+    }
+  };
+
+  const retrieveImages = (imageList) => {
+    setImageList(uploadedImageList => [...imageList]);
   };
 
   //track marker color selections
@@ -136,12 +160,31 @@ const AddCoordinateForm = (props) => {
       {!catchInformation ?
           null 
         : 
-        <SpeciesInformtionCard 
+        <SpeciesInformationCard 
           catchInformation={catchInformation}
         />
       }
       <div>ADD CLICKABLE IMAGES OF FISH THAT ARE AVAILABLE IN THE REGION</div>
       <div>ADD A BUTTON THAT ALLOWS USER TO ADD FISH TO THAT LIST OR REMOVE A CUSTOM ENTRY FISH FROM THE LIST</div>
+      {!toggleViewStates.renderFileDrop ?         
+        <GenericButton 
+          action={
+            () => {
+              props.updateTabState(null, null, props.activeCoordinate);
+              toggleViews('FILE_DROP');
+            }
+          }
+          name={'Upload Photo(s)'}
+        />
+        :
+        <ImageDropContainer 
+          retrieveImages={retrieveImages}
+          component={'location'}
+          images={uploadedImageList}
+          render={toggleViews}
+          renderStatus={toggleViewStates}
+        />
+      }
       <div id='location-button-container' style={locationContainerStyles}>
         <SaveButton
           save={captureValue}
@@ -164,14 +207,6 @@ const AddCoordinateForm = (props) => {
             }
           }
           name={'Add Catch'}
-        />
-        <GenericButton 
-          action={
-            () => {
-              props.updateTabState(null, null, props.activeCoordinate);
-            }
-          }
-          name={'Upload Photo(s)'}
         />
       </div>
     </div>
